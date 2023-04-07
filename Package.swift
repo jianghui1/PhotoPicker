830e58aee1a2282e415f2a7df43647e00b0ebbfe
jzh <jiangzenghui005@163.com>
2023-04-07 10:14:50 +0800

    Package Version from 5.8 to 5.7

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/jianghui1/CombineExtensions.git", branch: "main"),
+        .package(url: "https://github.com/jianghui1/CombineExtensions.git", revision: "9fdb593fcc5fe8277edaf43006ad11b4dee4af06"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
